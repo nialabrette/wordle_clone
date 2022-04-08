4d7f5338ddfd5abae03ad806cb041f2bb8b0af42
Nia <83649458+nialabrette@users.noreply.github.com>
2022-04-07 21:25:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,91 @@
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
+const messageDisplay = document.querySelector('.message-container')
 
-const keys = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','ENTER','<<']
+const wordle = 'SUPER'
+const keys = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','ENTER','Z','X','C','V','B','N','M','<<']
+const guessRows = [
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','','']
+]
+let currentRow = 0
+let currentTile = 0
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+    guessRow.forEach((guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+        rowElement.append(tileElement)
+        tileElement.classList.add('tile')
+    })
+    tileDisplay.append(rowElement)
+})
+
+const handleClick = (key) => {
+    console.log('clicked', key)
+    if(key === '<<'){
+        deleteLetter()
+        console.log('guessRows',guessRows)
+        return
+    }
+    if (key === 'ENTER'){
+        checkRow()
+        console.log('guessRows',guessRows)
+        return
+    }
+    addLetter(key)
+}
+const addLetter = (letter) => {
+    if (currentTile < 5 && currentRow < 6){
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+        tile.textContent = letter
+        guessRows[currentRow][currentTile] = letter
+        tile.setAttribute('data', letter)
+        currentTile++
+        console.log('guessRows',guessRows)
+    }
+    
+}
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', () => handleClick(key))
+    keyboard.append(buttonElement)
+})
+
+//delete letters
+const deleteLetter = () => {
+    if (currentTile > 0){
+        currentTile--
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+        tile.textContent = ''
+        guessRows[currentRow][currentTile] = ''
+        tile.setAttribute('data','')
+    }    
+}
+
+//enter
+const checkRow = () => {
+   const guess = guessRows[currentRow].join('')
+
+    if (currentTile === 5){
+        if (wordle == guess){
+            showMessage('Magnificent!')
+        }
+    }
+}
+
+const showMessage = (message) => {
+    const messageElement = document.createElement('p')
+    messageElement.textContent = message
+    messageDisplay.append(messageElement)
+    setTimeout(() => messageDisplay.removeChild(messageElement), 1800)
+}
